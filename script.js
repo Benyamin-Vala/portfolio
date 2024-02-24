25ec0b5805b973982379f819eef4529bae5d5fdb
Benyamin <102605148+Benyamin-Vala@users.noreply.github.com>
2024-02-24 22:16:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,200 @@
+document.getElementById("all-projects").addEventListener("click", () => {
+  var Allprojects = document.querySelectorAll(".project-cart");
+  Allprojects.forEach((element) => {
+    element.style.display = "block";
+  });
+  document.getElementById("websites-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("websites-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("all-projects").style.borderColor =
+    "var(--primary-border-color)";
+  document.getElementById("all-projects").style.backgroundColor =
+    "var(--primary-background-color)";
+  document.getElementById("api-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("api-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("python-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("python-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("csharp-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("csharp-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("cplusplus-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("cplusplus-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+});
+document.getElementById("websites-projects").addEventListener("click", () => {
+  var Allprojects = document.querySelectorAll(".project-cart");
+  var projects = document.querySelectorAll(".web-project");
+  Allprojects.forEach((element) => {
+    element.style.display = "none";
+  });
+  projects.forEach((element) => {
+    element.style.display = "block";
+  });
+  document.getElementById("websites-projects").style.borderColor =
+    "var(--primary-border-color)";
+  document.getElementById("websites-projects").style.backgroundColor =
+    "var(--primary-background-color)";
+  document.getElementById("all-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("all-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("api-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("api-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("python-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("python-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("csharp-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("csharp-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("cplusplus-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("cplusplus-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+});
+document.getElementById("api-projects").addEventListener("click", () => {
+  var Allprojects = document.querySelectorAll(".project-cart");
+  var projects = document.querySelectorAll(".api-project");
+  Allprojects.forEach((element) => {
+    element.style.display = "none";
+  });
+  projects.forEach((element) => {
+    element.style.display = "block";
+  });
+  document.getElementById("websites-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("websites-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("all-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("all-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("api-projects").style.borderColor =
+    "var(--primary-border-color)";
+  document.getElementById("api-projects").style.backgroundColor =
+    "var(--primary-background-color)";
+  document.getElementById("python-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("python-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("csharp-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("csharp-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("cplusplus-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("cplusplus-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+});
+document.getElementById("python-projects").addEventListener("click", () => {
+  var Allprojects = document.querySelectorAll(".project-cart");
+  var projects = document.querySelectorAll(".python-project");
+  Allprojects.forEach((element) => {
+    element.style.display = "none";
+  });
+  projects.forEach((element) => {
+    element.style.display = "block";
+  });
+  document.getElementById("websites-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("websites-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("all-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("all-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("api-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("api-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("python-projects").style.borderColor =
+    "var(--primary-border-color)";
+  document.getElementById("python-projects").style.backgroundColor =
+    "var(--primary-background-color)";
+  document.getElementById("csharp-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("csharp-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("cplusplus-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("cplusplus-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+});
+document.getElementById("csharp-projects").addEventListener("click", () => {
+  var Allprojects = document.querySelectorAll(".project-cart");
+  var projects = document.querySelectorAll(".csharp-project");
+  Allprojects.forEach((element) => {
+    element.style.display = "none";
+  });
+  projects.forEach((element) => {
+    element.style.display = "block";
+  });
+  document.getElementById("websites-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("websites-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("all-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("all-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("api-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("api-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("python-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("python-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("csharp-projects").style.borderColor =
+    "var(--primary-border-color)";
+  document.getElementById("csharp-projects").style.backgroundColor =
+    "var(--primary-background-color)";
+  document.getElementById("cplusplus-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("cplusplus-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+});
+document.getElementById("cplusplus-projects").addEventListener("click", () => {
+  var Allprojects = document.querySelectorAll(".project-cart");
+  var projects = document.querySelectorAll(".cplusplus-project");
+  Allprojects.forEach((element) => {
+    element.style.display = "none";
+  });
+  projects.forEach((element) => {
+    element.style.display = "block";
+  });
+  document.getElementById("websites-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("websites-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("all-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("all-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("api-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("api-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("python-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("python-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("csharp-projects").style.borderColor =
+    "var(--secondary-border-color)";
+  document.getElementById("csharp-projects").style.backgroundColor =
+    "var(--secondary-background-color)";
+  document.getElementById("cplusplus-projects").style.borderColor =
+    "var(--primary-border-color)";
+  document.getElementById("cplusplus-projects").style.backgroundColor =
+    "var(--primary-background-color)";
+});
